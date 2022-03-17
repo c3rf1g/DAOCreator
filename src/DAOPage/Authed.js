f8c82e95e151394ab45aa96907abc6b992deada6
c3rf1g <c3rf1g@gmail.com>
2022-03-17 20:51:48 +0300

    auth fix and loading screen

--- src/DAOPage/Authed.js
+++ src/DAOPage/Authed.js
@@ -1,13 +1,66 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import "./DAOPage.css"
+import { BarLoader,DoubleBubble, SlidingPebbles }
+    from 'react-spinner-animated';
 
-function Authed() {
+import 'react-spinner-animated/dist/index.css'
+import {LinearProgress} from "@mui/material";
+import Web3 from "web3";
 
-    return <div style={{backgroundColor: "white"}}>VSE OK)</div>;
+
+function Authed(props) {
+    const address = props.DAOAddress
+    const [isDao, setIsDao] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const isDaoContract = () => {
+        const web3 = new Web3(Web3.givenProvider)
+        const abi = [{"inputs":[{"internalType":"address","name":"_address","type":"address"}],"name":"getAirdropStatusByAddress","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]
+        try {
+            const contract = new web3.eth.Contract(abi, address)
+            contract.methods.getAirdropStatusByAddress(props.account).call().then(r => {
+                    console.log(r)
+                    setIsDao(true)
+                    setLoading(false)
+                }
+            )
+        } catch {
+            setIsDao(false)
+            setLoading(false)
+        }
+    }
+    useEffect(() => {
+        if (loading) {
+            isDaoContract()
+
+        }
+    }, [loading])
+    return (
+        <div>
+            {
+                loading ?
+                    <div className="ErrorMessage" style={{backgroundColor: "white"}}>
+                        <LinearProgress />
+                        <p>Loading</p>
+                    </div>
+                    :
+                        !isDao && !loading  ?
+                            <div className="ErrorMessage" style={{backgroundColor: "white" }}>You are not in DAO :(</div>
+                        :
+
+                            <div className="ErrorMessage" style={{backgroundColor: "white"}}>
+                                <LinearProgress />
+                                <p>Loading DAO</p>
+                            </div>
+            }
+        </div>
+
+
+    )
 }
 
-const Auth = () => {
+const Auth = (props) => {
     const [auth, setAuth] = useState(false)
+    const [accountAddress, setAccountAddress] = useState("")
     const Auth =  () => {
         if (window.ethereum) {
             const ethereum = window.ethereum
@@ -17,6 +70,7 @@ const Auth = () => {
                 .then((r) => {
                     console.log(r[0])
                     localStorage.setItem("authed", true)
+                    setAccountAddress(r[0])     
                     setAuth(true)
                 })
                 .catch((err) => {
@@ -35,7 +89,7 @@ const Auth = () => {
         <div>
             {
                 auth || localStorage.getItem('authed')?
-                    <Authed/>
+                    <Authed account={accountAddress} DAOAddress={props.DAOAddress}/>
                     :
                     <div className="Button">
                         <button className="ConnectWallet" onClick={Auth}>Connect wallet</button>
